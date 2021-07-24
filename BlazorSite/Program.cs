892f220def2fbd273529c1793bc0a1cb94dfdd1b
Fabian Figueredo <fabek128@gmail.com>
2021-07-24 17:51:33 -0300

    New services added in program.cs

--- BlazorSite/Program.cs
+++ BlazorSite/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BlazorSite.Services;
+using BlazorSite.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorSite
 {
@@ -19,6 +21,8 @@ namespace BlazorSite
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             builder.Services
                 .AddScoped<IAccountService, AccountService>()
